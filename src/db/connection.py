84ab1357af266221f5af564a532330baf2d50261
Eleftheria Pardali <pardalieri@gmail.com>
2025-11-16 16:51:45 +0200

    Create connection.py

---
+++ src/db/connection.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+
+def get_engine():
+    db_user = "postgres"
+    db_pass = "postgres"
+    db_host = "localhost"
+    db_port = "5432"
+    db_name = "loan_portfolio_db"
+
+    connection_url = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+    engine = create_engine(connection_url)
+    return engine
