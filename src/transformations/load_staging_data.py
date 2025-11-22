b84dced23766dd26b07461910bc319891c04e578
Eri Pardali <pardalieri@gmail.com>
2025-11-22 15:46:16 +0200

    loan_staging_data.py

--- src/transformations/load_staging_data.py
+++ src/transformations/load_staging_data.py
@@ -1,33 +1,60 @@
 import csv
-from datetime import datetime
 from pathlib import Path
 from src.db.connection import get_engine
 
-RAW_CSV = Path("data/loan_raw.csv")
+
+STAGING_CSV = Path("data/processed/loan_kpi_staging.csv")
 
 
 def load_into_staging():
     engine = get_engine()
-    print("Loading cleaned data into staging.loan_applications ...")
-    
+    print("Loading KPI dataset into staging.loan_applications ...")
+
     sql = """
-        COPY staging.loan_applications(
-            loan_id,
+        COPY staging.loan_applications (
+            id,
             member_id,
             loan_amnt,
+            funded_amnt,
             int_rate,
-            loan_status,
-            grade,
+            installment,
             sub_grade,
-            issue_d
+            issue_d,
+            issue_d_year,
+            issue_d_month,
+            annual_inc,
+            dti,
+            emp_length_years,
+            addr_state,
+            revol_bal,
+            revol_util,
+            credit_history_age_months,
+            total_pymnt,
+            total_rec_prncp,
+            total_rec_int,
+            recoveries,
+            last_pymnt_d,
+            last_pymnt_amnt,
+            payment_to_loan_ratio,
+            income_to_loan_ratio,
+            grade_ord,
+            sub_grade_ord,
+            loan_status_binary
         )
         FROM STDIN
-        WITH CSV HEADER DELIMITER ',' QUOTE '\"';
+        WITH CSV HEADER DELIMITER ',' QUOTE '\"'
     """
-    
+
     with engine.raw_connection() as conn:
-        with conn.cursor() as cur, RAW_CSV.open("r", encoding="utf-8") as f:
+        cur = conn.cursor()
+
+        with open(STAGING_CSV, "r", encoding="utf-8") as f:
             cur.copy_expert(sql, f)
+
         conn.commit()
 
-    print("Staging load complete.")
+    print("Staging load complete ✔️")
+    
+
+if __name__ == "__main__":
+    load_into_staging()
