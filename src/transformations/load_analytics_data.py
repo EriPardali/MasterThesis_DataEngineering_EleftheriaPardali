ca6663ebe9861e337ebe960b65b4b73c6fd228f7
Eri Pardali <pardalieri@gmail.com>
2025-11-23 18:01:00 +0200

    load_analytics_data.py

---
+++ src/transformations/load_analytics_data.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from pathlib import Path
+from src.db.connection import get_engine
+
+def load_analytics_data():
+    engine = get_engine()
+
+    print("Reading from staging.loan_portfolio_features ...")
+    df = pd.read_sql("SELECT * FROM staging.loan_portfolio_features", engine)
+
+    print("Writing to analytics.loan_portfolio_features ...")
+    df.to_sql(
+        name="loan_portfolio_features",
+        schema="analytics",
+        con=engine,
+        if_exists="replace",
+        index=False
+    )
+
+    print("Completed loading into analytics.loan_portfolio_features.")
+
+if __name__ == "__main__":
+    load_analytics_data()
